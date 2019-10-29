0eda37d164ed4ec23b356115ef8dbf697dbd4bba
the 14th web <dennis.itua1@gmail.com>
2019-10-28 20:25:52 -0400

    updating js and log.txt details

--- liri.js
+++ liri.js
@@ -9,23 +9,23 @@ var Spotify = require('node-spotify-api');
 
 var axios = require("axios")
 
-// var omdb = require("omdb");
-
 var fs = require("fs");
 
 // bandsintown
-var getMeBands = function(artist){
+var getBand = function(artist){
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response){
       var concertDate = response.data[0].datetime;
       
       var concertInfo =
-      `======================================
+            `
+      ============================================       
       Venue: ${response.data[0].venue.name}
       Venue Location: ${response.data[0].venue.city}, ${response.data[0].venue.country}
-      date of the Event: ${moment(concertDate).format('dddd, MMMM Do YYYY, h:mm:ss a')}`
+      date of the Event: ${moment(concertDate).format('dddd, MMMM Do YYYY, h:mm:ss a')}
+      ============================================`
 
-    //   datalog("Concert Searched", artist , concertInfo)
-      console.log(concertInfo);
+      logtxt("CONCERT SEARCHED: "+ artist + concertInfo)
+    //   console.log(concertInfo);
     })
 }
 
@@ -35,7 +35,7 @@ var getArtistNames = function(artist) {
     return artist.name;
 }
 
-var getMeSpotify = function(songName) {
+var getSpotify = function(songName) {
     
     var spotify = new Spotify(keys.spotify);
 
@@ -56,8 +56,8 @@ var getMeSpotify = function(songName) {
             Album: ${songs[i].album.name}
             ==================================================
             ==================================================`
-            // datalog("Song Searched", songName , songInfo)
-            console.log(songInfo);
+            logtxt("SONG SEARCHED: " + songName + songInfo)
+            // console.log(songInfo);
             
         }
     })
@@ -65,7 +65,7 @@ var getMeSpotify = function(songName) {
 
 
 // movie omdb
-var getMeMovie = function(movieName) {
+var getMovie = function(movieName) {
 
     if (!movieName){
         movieName = "Mr.Nobody"
@@ -73,29 +73,41 @@ var getMeMovie = function(movieName) {
 
     axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(function(response){
     var movieInfo =
-   `    ===================================================
+   `    
+         ===================================================
     Title: ${response.data.Title}
-    ===================================================
+    
     Year: ${response.data.Year}
-    ===================================================
+    
     Rated: ${response.data.Rated}
-    ===================================================
+    
     IMDB Rating: ${response.data.imdbRating}
-    ===================================================
+   
     Rotten Tomatoes Rating: ${response.data.Ratings[0].Value}
-    ===================================================
+    
     Country: ${response.data.Country}
-    ===================================================
+   
     Language: ${response.data.Language}
-    ===================================================
+   
     Plot: ${response.data.Plot}
-    ===================================================
+   
     Actors: ${response.data.Actors}
     ===================================================`
-        // datalog("Movie Searched", movieName, movieInfo)
-        console.log(movieInfo)
+    logtxt("MOVIE SEARCHED: " + movieName + movieInfo)
+        // console.log(movieInfo);
     })
 };
+
+
+var logtxt = function(data) {
+    console.log(data)
+
+    fs.appendFile("log.txt", "\r\n" + data, function (err){
+        if(err){
+            return console.log(err)
+        } 
+    });
+}
 
 var doWhatItSays = function(){
 
@@ -106,40 +118,28 @@ var doWhatItSays = function(){
         
         var dataArr = data.split(",")
 
-        runAction(dataArr[0], dataArr[1])
+        dataDisplay(dataArr[0], dataArr[1])
     });
 }
 
 
-
-var runAction = function(func, parm) {
+var dataDisplay = function(func, parm) {
     switch (func) {
         case "concert-this":
-            getMeBands(parm)
+            getBand(parm)
             break
         case "spotify-this-song":
-            getMeSpotify(parm)
+            getSpotify(parm)
             break
         case "movie-this":
-            getMeMovie(parm)
+            getMovie(parm)
             break
         case "do-what-it-says":
             doWhatItSays()
             break
         default:
-            outputData("That is not a command that I recognize, please try again.") 
+            displayDefault("Command not recognized, please try again.") 
     }
 }
 
-
-var outputData = function(data) {
-    console.log(data)
-
-    fs.appendFile("log.txt", "\r\n" + data, function (err){
-        if(err){
-            return console.log(err)
-        } 
-    })
-}
-
-runAction(process.argv[2], process.argv[3]);
+dataDisplay(process.argv[2], process.argv[3]);
